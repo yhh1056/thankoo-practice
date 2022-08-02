5502044b16338721a916fb25192f5c3c1cd233e1
yhh1056 <yhh1056@naver.com>
2022-08-02 21:29:57 +0900

    first commit

---
+++ src/test/java/practice/thankoo/ThankooApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.thankoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThankooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
